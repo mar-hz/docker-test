0e9f0a81d516ef36fd247af6ded721c9332fccec
Mari <a01285878@tec.mx>
2025-08-14 14:13:04 -0600

    minor python change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return jsonify({"message": "yippie yippie yippie yippie"})
+	return jsonify({"message": "yippie yippie yippie"})
 
 if __name__ == "__main__":
 	app.run(threaded=True, host='0.0.0.0', port=3000)
