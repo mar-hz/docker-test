7d2b719b4e7d6aee08d79fdeaf404a02752a13a5
Mari <a01285878@tec.mx>
2025-09-02 16:54:59 -0600

    added GET /news

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
-@app.route("/")
+news = [{"id": 0, "title": "", "content": ""}]
+next_id = 1
+
+@app.route("/", methods=["GET"])
 def index():
-	return jsonify({"message": "yippie yippie yippie yippiee"})
+	pass
+
+@app.route("/news", methods=["GET"])
+def list_news():
+	return jsonify({"count": len(news), "items": news})
+
+@app.route("/news", methods=["POST"])
+def create_news():
+	pass
+
+@app.route("/news/<int:item_id>", methods=["POST"])
+def update_news():
+	pass
+
+@app.route("/news/<int:item_id>", methods=["DELETE"])
+def delete_news():
+	pass
 
 if __name__ == "__main__":
 	app.run(threaded=True, host='0.0.0.0', port=3000)
