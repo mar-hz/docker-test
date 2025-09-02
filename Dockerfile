7bc4bb52274ea5b49613390bb31ae0f3782d8d18
Mari <a01285878@tec.mx>
2025-09-02 17:32:33 -0600

    Added CRUD

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM python:latest
 
+ENV PYTHONUNBUFFERED=1 PIP_NO_CACHE_DIR=1
+
 WORKDIR /app
 COPY . .
-RUN pip install Flask
+RUN pip install flask Flask-BasicAuth
 
 EXPOSE 3000
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify, request, abort
+from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'marian'
+app.config['BASIC_AUTH_PASSWORD'] = 'matrix'
+
+basic_auth = BasicAuth(app)
 
 news = [{"id": 0, "title": "", "content": ""}]
 next_id = 1
@@ -14,16 +19,26 @@ def list_news():
 	return jsonify({"count": len(news), "items": news})
 
 @app.route("/news", methods=["POST"])
+@basic_auth.required
 def create_news():
-	pass
+	data = request.json
+	return jsonify(data), 201
 
 @app.route("/news/<int:item_id>", methods=["POST"])
-def update_news():
-	pass
+@basic_auth.required
+def update_news(item_id: int):
+	item = news[item_id]
+	data = request.json
+	for key in ("title", "content"):
+		if key in data:
+			item[key] = data[key]
+	return jsonify(item)
 
 @app.route("/news/<int:item_id>", methods=["DELETE"])
-def delete_news():
-	pass
+@basic_auth.required
+def delete_news(item_id: int):
+	del news[item_id]
+	return jsonify({"status": "deleted", "id": item_id})
 
 if __name__ == "__main__":
 	app.run(threaded=True, host='0.0.0.0', port=3000)
